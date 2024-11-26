e5758bcd6f40ab63613dfbc4e8f7ed2354db7697
nguyenscg <nguyencsg@gmail.com>
2024-11-26 13:04:24 -0600

    initial file setup 2

---
+++ number-guessing.py
@@ -0,0 +1,11 @@
+import random
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+print("Choose a difficulty. Type 'easy' or 'hard':")
+
+# Easy should have 10 attempts, Hard should be 5 attempts
+
+print("Make a guess: ")
+
+# Too high or too low against the user's guess
