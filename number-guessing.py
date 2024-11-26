391cacee23d95a01aa34ce003640030c51500889
nguyenscg <nguyencsg@gmail.com>
2024-11-26 13:09:35 -0600

    generate random number that computer is thinking of. setup user's guess to be int

--- number-guessing.py
+++ number-guessing.py
@@ -1,11 +1,16 @@
 import random
 print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
+print("I'm thinking of a number between 1 and 100. ")
+
+# The answer from 1 to 100
+computer_answer = random.randint(1, 100)
+print(computer_answer)
 
 print("Choose a difficulty. Type 'easy' or 'hard':")
 
 # Easy should have 10 attempts, Hard should be 5 attempts
 
-print("Make a guess: ")
+# User's guess, will only take int
+guess = int(input("Make a guess: "))
 
 # Too high or too low against the user's guess
