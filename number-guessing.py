2ef2ce3cd6698a5d07e64271f29951b5158de6aa
nguyenscg <nguyencsg@gmail.com>
2024-11-26 13:18:01 -0600

    created a function to check the user's guess  against the actual answer

--- number-guessing.py
+++ number-guessing.py
@@ -14,3 +14,11 @@ print("Choose a difficulty. Type 'easy' or 'hard':")
 guess = int(input("Make a guess: "))
 
 # Too high or too low against the user's guess
+
+def check_answer(user_guess, the_actual):
+    if user_guess > the_actual:
+        print("Too high!")
+    elif user_guess < the_actual:
+        print("Too low!")
+    else:
+        print("You got it!")
