49990ea3a2e3cd75d229d574791c7ab15cbffd11
nguyenscg <nguyencsg@gmail.com>
2024-12-14 14:23:21 -0600

    created a set difficulty function to set level

--- number-guessing.py
+++ number-guessing.py
@@ -8,6 +8,15 @@ difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 EASY_LEVEL = 10
 HARD_LEVEL = 5
 
+# Create a function that sets the difficulty
+def set_difficulty():
+    if difficulty == "easy":
+        print(EASY_LEVEL)
+        return EASY_LEVEL
+    else:
+        return HARD_LEVEL
+set_difficulty()
+
 # User chooses a level, easy = 10 attempts, hard = 5 attempts
 # Prompt 'Make a guess' input
 # Compare guess if its lower or higher
