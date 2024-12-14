25eac5578da54f6226bf3afbccacad6646a1549a
nguyenscg <nguyencsg@gmail.com>
2024-12-14 15:46:42 -0600

    save computer's number into a variable and pass it through compare function to compare the guess and answer

--- number-guessing.py
+++ number-guessing.py
@@ -13,17 +13,30 @@ def set_difficulty():
         return HARD_LEVEL
 
 # Create a function that compares the guess to the number that the computer is thinking, pass in number input
-def compare(guessed):
-    computer_number = random.randint(1, 100)
-    if guessed == computer_number:
-        print(f"You guessed correctly! it is {computer_number}")
+def compare(guessed, answer):
+    if guessed == answer:
+        print(f"You guessed correctly! it is {answer}")
+    elif guessed > answer:
+        print("Too high!")
+    elif guessed < answer:
+        print("Too low!")
 
 def game():
     # Print Welcome and Message statements
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
     # Set the difficulty based on the user's level selection, this will trigger the set_difficulty function and store the amount of attempts based on level
-    level = set_difficulty()
+    turns = set_difficulty()
+
+    # store random number that the computer is thinking of
+    computer_number = random.randint(1, 100)
+
+    # You have xx atempts remaining to guess the number
+    print(f"You have {turns} attempts to guess the number.")
+    # Prompt 'Make a guess'
+    guess = int(input("Make a guess: "))
+    # Compare the guess to computer's number, if it's wrong -1 but allow the user to guess again
+    print(compare(guess, computer_number))
 
 game()
 
